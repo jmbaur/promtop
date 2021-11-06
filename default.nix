65f6e9ebc9dd28666c5ed03aae97435f7aee0546
Jared Baur <jaredbaur@fastmail.com>
2021-11-06 16:14:15 -0700

    Add build file for Nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { }, system ? builtins.currentSystem }:
+let
+  zig-overlay = builtins.fetchTarball "https://github.com/arqv/zig-overlay/archive/main.tar.gz";
+  zig = (import zig-overlay { inherit pkgs system; }).master.latest;
+in
+pkgs.stdenv.mkDerivation {
+  pname = "pomtop";
+  version = "0.1.0";
+  src = builtins.path { path = ./.; };
+  nativeBuildInputs = [ pkgs.ncurses6 zig ];
+  preBuild = ''
+    export HOME=$TMPDIR
+  '';
+  installPhase = ''
+    zig build -Drelease-safe -Dcpu=baseline --prefix $out install
+  '';
+}
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> { }, system ? builtins.currentSystem }:
-let
-  zig-overlay = builtins.fetchTarball "https://github.com/arqv/zig-overlay/archive/main.tar.gz";
-  zig = (import zig-overlay { inherit pkgs system; }).master.latest;
-in
-pkgs.mkShell {
-  buildInputs = [ pkgs.ncurses6 zig ];
-}
