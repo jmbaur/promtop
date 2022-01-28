92099e869b93de714bd98864e029ed3833c5ecb7
Jared Baur <jaredbaur@fastmail.com>
2022-01-27 18:47:52 -0800

    Make flake app

--- flake.nix
+++ flake.nix
@@ -2,32 +2,44 @@
   description = "promtop";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.11-small";
+    nixpkgs.url = "nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:arqv/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, flake-utils, zig }@inputs: {
+    overlay = final: prev: {
+      promtop = nixpkgs.legacyPackages.${prev.system}.stdenvNoCC.mkDerivation {
+        pname = "promtop";
+        version = "0.1.0";
+        buildInputs = [ zig.packages.${prev.system}.master.latest ];
+        src = builtins.path { path = ./.; };
+        preBuild = ''
+          export HOME=$TMPDIR
+        '';
+        installPhase = ''
+          zig build --prefix $out install
+        '';
+      };
+    };
+  }
+  // flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        overlays = [ self.overlay ];
+        inherit system;
+      };
+    in
+    with pkgs; rec {
+      devShell = mkShell {
         buildInputs = [ zig.packages.${system}.master.latest ];
-      in
-      with pkgs; rec {
-        devShell = mkShell { inherit buildInputs; };
-        packages.promtop = stdenv.mkDerivation {
-          pname = "promtop";
-          version = "0.1.0";
-          inherit buildInputs;
-          src = builtins.path { path = ./.; };
-          preBuild = ''
-            export HOME=$TMPDIR
-          '';
-          installPhase = ''
-            zig build --prefix $out install
-          '';
-        };
-        defaultPackage = packages.promtop;
-        overlay = self: super: { promtop = packages.promtop; };
-      });
+      };
+      packages.promtop = pkgs.promtop;
+      defaultPackage = pkgs.promtop;
+      apps.promtop = flake-utils.lib.mkApp {
+        drv = pkgs.promtop;
+        name = "promtop";
+      };
+      defaultApp = apps.promtop;
+    });
 }
